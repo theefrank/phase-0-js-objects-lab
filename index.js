c435c8012b2604aff4f14f4dfe93e86fdac02b12
frank mwangi <frankmwangi2007@gmail.com>
2026-04-17 11:48:59 +0300

    objects

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
 //Write your code here
+//Write your code here
+// Task 2: Create the attendee object
+let attendee = {
+    attendeeId: "T001",
+    name: "Alice Smith",
+    event: "JavaScript Conference",
+    ticketType: "VIP",
+    ticketPrice: 150.00
+};
 
+
+// Task 3: Log attendee name
+function logAttendeeName(attendee) {
+    console.log(attendee.name);
+}
+
+
+// Task 4: Log ticket price
+function logTicketPrice(attendee) {
+    console.log(attendee.ticketPrice);
+}
+
+
+// Task 5: Update ticket type
+function updateTicketType(attendee, newType) {
+    attendee.ticketType = newType;
+}
+
+
+// Task 6: Update ticket price
+function updateTicketPrice(attendee, newPrice) {
+    attendee.ticketPrice = newPrice;
+}
+
+
+// Task 7: Remove event property
+function removeEventProperty(attendee) {
+    delete attendee.event;
+}
+
+
+// Task 8: Add checkedIn property
+function addCheckedInProperty(attendee) {
+    attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
@@ -12,3 +56,16 @@ module.exports = {
   ...(typeof removeEventProperty !== 'undefined' && { removeEventProperty }),
   ...(typeof addCheckedInProperty !== 'undefined' && { addCheckedInProperty })
 };
+
+
+
+logAttendeeName(attendee); // Alice Smith
+logTicketPrice(attendee); // 150
+
+updateTicketType(attendee, "Regular");
+updateTicketPrice(attendee, 100);
+
+removeEventProperty(attendee);
+addCheckedInProperty(attendee);
+
+console.log(attendee);
